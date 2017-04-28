094138971fd8717b37ef7e38263e2b0a9990e288
Salian <git@salian.me>
2017-04-28 13:40:06 +0800

    上传场景美术资源 场景预制体等资源位于根目录

---
+++ KillingMore/Assets/Editor/ToPrefabs.cs
@@ -0,0 +1,25 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+public class ToPrefabs : EditorWindow {
+
+    [MenuItem("EditorTool/ToPrefabs")]
+
+    public static void ShowWindows()
+    {
+        GetWindow(typeof(ToPrefabs));
+    }
+    private void OnGUI()
+    {
+        if (GUILayout.Button("To Prefabs", GUILayout.Width(255)))
+        {
+            Go2Prefabs();
+        }
+    }
+    private void Go2Prefabs()
+    {
+        if(Selection.activeGameObject!=null)
+        PrefabUtility.CreatePrefab("Assets/"+ Selection.activeGameObject.name+ ".prefab", Selection.activeGameObject, ReplacePrefabOptions.Default);
+        
+    }
+}
