57abafe1cf630180b3df44a41b917358550c8844
SALIAN-APC\Salian <leverlup@qq.com>
2017-04-30 21:49:02 +0800

    更新策划文档以及表格

--- KillingMore/Assets/StreamingAssets/Region.r
+++ KillingMore/Assets/StreamingAssets/Region.r
@@ -1,2 +1,2 @@
-{"Name":"Character_Prefab_1000","ActorType":"Character","X":-1.0,"Y":0.0,"Z":0.0,"Rot":0.0}
-{"Name":"Character_Prefab_1000","ActorType":"Character","X":1.0,"Y":0.0,"Z":0.0,"Rot":0.0}
+{"Name":"RoomA","ActorType":"Obstacle","PosX":-0.248297095298767,"PosY":-1.12999999523163,"PosZ":1.03601598739624,"RotX":87.8092956542969,"RotY":180.0,"RotZ":180.0}
+{"Name":"Character_Prefab_1000","ActorType":"Character","PosX":-1.0,"PosY":0.0,"PosZ":0.0,"RotX":0.0,"RotY":0.0,"RotZ":0.0}
